cb7ec0f5b6f7f3d76cc349ee22d6430fdab363dc
Leonardo de Souza Oliveira <dev7@aproms.com.br>
2025-12-01 17:02:18 -0400

    first commit

---
+++ src/controllers/users-controller.ts
@@ -0,0 +1,23 @@
+import { Request, Response } from "express"
+import { prisma } from "../prisma"
+
+class UsersController {
+  async index(request: Request, response: Response) {
+    return response.json()
+  }
+
+  async create(request: Request, response: Response) {
+    const { name, email } = request.body
+
+    await prisma.user.create({ data: { name, email } })
+
+    
+    return response.status(201).json()
+  }
+
+  async show(request: Request, response: Response) {
+    return response.json()
+  }
+}
+
+export { UsersController }
